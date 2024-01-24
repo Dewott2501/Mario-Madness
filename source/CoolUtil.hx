603cc3eb7fe283d3e089b9b75b1031dddb1378a4
CHIPI34 <157554164+CHIPI34@users.noreply.github.com>
2024-01-23 23:24:16 -0300

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -63,7 +63,7 @@ class CoolUtil {
 
 	public static function coolTextFile(path:String):Array<String> {
 		var daList:Array<String> = [];
-		#if sys
+		#if windows 
 		if (FileSystem.exists(path))
 			daList = File.getContent(path).trim().split('\n');
 		#else
