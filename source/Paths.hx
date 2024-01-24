2f4f69c1afe4072542ca3b4861ee23e6bebd44d2
CHIPI34 <157554164+CHIPI34@users.noreply.github.com>
2024-01-23 23:58:47 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -247,8 +247,8 @@ class Paths
 
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
 	{
-		#if sys
-		#if MODS_ALLOWED
+		#if windows 
+		#if windows 
 		if (!ignoreMods && FileSystem.exists(modFolders(key)))
 			return File.getContent(modFolders(key));
 		#end
