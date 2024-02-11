b6a3e710298a8b04e04d00690a64240d676bfca5
Redar13 <93761434+Redar13@users.noreply.github.com>
2024-02-11 17:08:17 +0500

    Revert "Update complations_preset.bat"
    This reverts commit 6c18915876bdce0876fcfbeda65ad9d2ad843b8a.

--- complations_preset.bat
+++ complations_preset.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal
 
-haxelib install flixel-addons 3.1.1
-haxelib install flixel 5.3.1
-haxelib install flixel-ui 2.5.0
-haxelib install flixel-tools 1.5.1
-haxelib install hxCodec 2.6.1
-haxelib install lime 8.0.2
-haxelib install openfl 9.2.0
+haxelib set flixel-addons 3.1.1
+haxelib set flixel 5.3.1
+haxelib set flixel-ui 2.5.0
+haxelib set flixel-tools 1.5.1
+haxelib set hxCodec 2.6.1
+haxelib set lime 8.0.2
+haxelib set openfl 9.2.0
 
 echo Config ready
 echo Do you want to compile? (Y/N)
